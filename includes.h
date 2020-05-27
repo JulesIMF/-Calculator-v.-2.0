97a01559feef016468323f8c364e40c0908de36c
JulesIMF <intermasterfly@yandex.ru>
2020-05-27 19:43:07 +0500

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <clocale>
+#include <cstdlib>
+#include <cctype>
+#include <map>
+#include <list>
+
+using namespace std;
